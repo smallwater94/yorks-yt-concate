6da6fc6a99840c91c5325d8ff5d7ae0ac7fac7fb
York Wu <smallwater94@gmail.com>
2021-07-19 22:07:32 +0800

    Successful got video urls from channel ---by York---

---
+++ yorks_yt_concate/main.py
@@ -0,0 +1,34 @@
+import urllib.request
+import json
+
+CHANNEL_ID = 'UCjXfkj5iapKHJrhYfAF9ZGg'
+
+def get_all_video_in_channel(CHANNEL_ID):
+    api_key = 'API_KEY'
+
+    base_video_url = 'https://www.youtube.com/watch?v='
+    base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
+
+    first_url = base_search_url+'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(api_key, CHANNEL_ID)
+
+    video_links = []
+    url = first_url
+    while True:
+        inp = urllib.request.urlopen(url)
+        resp = json.load(inp)
+
+        for i in resp['items']:
+            if i['id']['kind'] == "youtube#video":
+                video_links.append(base_video_url + i['id']['videoId'])
+
+        try:
+            next_page_token = resp['nextPageToken']
+            url = first_url + '&pageToken={}'.format(next_page_token)
+        except:
+            break
+    return video_links
+
+
+
+x = get_all_video_in_channel(CHANNEL_ID)
+print(len(x))
